88bea513dadce65e6a327585b16fa6e68018b535
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2023-12-07 19:09:29 +0300

    get data from initial state

--- web-sayfam/src/components/Skills.js
+++ web-sayfam/src/components/Skills.js
@@ -1,39 +1,35 @@
 import React from "react";
-import js from "../img/js.png";
-import react from "../img/react.png";
-import redux from "../img/redux.png";
-import node from "../img/node.png";
-import vscode from "../img/vscode.png";
-import figma from "../img/figma.png";
+
 import { useSelector } from "react-redux";
 const Skills = () => {
   const darkMode = useSelector(state => state.darkMode)
+  const skills = useSelector(state=> state.skillsData)
   return (
-    <div className={`w-full ${darkMode ? "bg-[#484148]": "bg-white"} `} >
+    <div className={`w-full ${darkMode ? "bg-[#484148] text-white": "bg-white"} `} >
       <div className="flex flex-col items-center pb-30 pt-30  lg-[.5rem] lg:px[0rem]  lg:h-[25rem] ">
         <h3 className="text-[3rem] lg:text-[3rem] font-['Inter'] font-[500]  lg:leading-[98.09px] lg:tracking-[0.01em]">
-          Skills
+         {skills[0].skillHeader}
         </h3>
         <br></br>
         <div className="flex flex-wrap flex-col gap-12 lg:flex-row">
           <div className="icon-div">
-            <img src={js} alt="" className="program-icon" />
+            <img src={skills[0].imageJs} alt="" className="program-icon" />
           </div>
           <div className="icon-div">
-            <img src={react} alt="" className="program-icon" />
+            <img src={skills[0].imageReact} alt="" className="program-icon" />
           </div>
           <div className="icon-div">
-            <img src={redux} alt="" className="program-icon" />
+            <img src={skills[0].imageRedux} alt="" className="program-icon" />
           </div>
 
           <div className="icon-div">
-            <img src={node} alt="" className="program-icon" />
+            <img src={skills[0].imageNode} alt="" className="program-icon" />
           </div>
           <div className="icon-div">
-            <img src={vscode} alt="" className="program-icon" />
+            <img src={skills[0].imageVs} alt="" className="program-icon" />
           </div>
           <div className="icon-div">
-            <img src={figma} alt="" className="program-icon" />
+            <img src={skills[0].imageFigma} alt="" className="program-icon" />
           </div>
         </div>
       </div>
